74de0f566c5b6b096e63fec560554adaa4dc0903
= <shuklashubham2277@gmail.com>
2022-11-02 15:32:30 +0530

    base case comment

--- gcd.cpp
+++ gcd.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // function to calculate gcd
 int gcd(int n1, int n2)
 {
+    // base case
+    // is first number is zero means we got our gcd value
     if(n1 == 0)return  n2;
     else return gcd(n2 % n1, n1);
 }
